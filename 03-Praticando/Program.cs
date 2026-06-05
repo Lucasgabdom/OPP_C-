59cb1f4ba516c2f05a2c102d2cfe64c2d27f70f3
Lucas Gabriel <gabriellucaswbtrombone@gmail.com>
2026-06-05 15:17:52 -0300

    Praticando POO

---
+++ 03-Praticando/Program.cs
@@ -0,0 +1,15 @@
+﻿Produto item1 = new Produto();
+item1.Nome= "Cadeira Gamer";
+item1.Descricao = "Cadeira Gamer com encosto reclinável e suporte lombar ajustável.";
+item1.Preco = 899.99m;
+item1.Estoque = 20;
+item1.exibirInformacoes();
+
+Produto item2 = new Produto();
+item2.Nome = "Mesa de Escritório";
+item2.Descricao = "Mesa de escritório com acabamento em madeira e compartimentos para armazenamento.";
+item2.Preco = 499.99m;
+item2.Estoque = 15;
+item2.exibirInformacoes();
+
+
