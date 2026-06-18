ac8956927bc160c757732b92d3a363f1058f931a
.\Lucasgabdom <gabriellucaswbtrombone@gmail.com.br>
2026-06-17 23:04:18 -0300

    Voltando a estudar back-end com c#

---
+++ livros/Program.cs
@@ -0,0 +1,14 @@
+﻿Livro meuLivro = new Livro();
+meuLivro.Titulo = "O Senhor dos Anéis";
+meuLivro.Autor = "J.R.R. Tolkien";
+
+Console.WriteLine("Livro: " + meuLivro.Titulo);
+Console.WriteLine("Autor: " + meuLivro.Autor);
+
+public class Livro
+{
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+}
+
+
