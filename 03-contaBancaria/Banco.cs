38324f11fbfd39b8b47e2acd430a8721cccc9f64
.\Lucasgabdom <gabriellucaswbtrombone@gmail.com.br>
2026-06-23 20:03:25 -0300

    Relembrando conceitos dentro do C#

---
+++ 03-contaBancaria/Banco.cs
@@ -0,0 +1,22 @@
+﻿ContaBancaria conta1 = new ContaBancaria("12345", 1000.0);
+Console.WriteLine();
+
+Console.WriteLine("Deseja fazer um depósito? (s/n)");
+
+string resposta = Console.ReadLine();
+
+if (resposta == "s")
+{
+    Console.WriteLine("Qual o valor do depósito?");
+    double valor = double.Parse(Console.ReadLine());
+
+    conta1.Depositar(valor);
+
+
+    Console.WriteLine($"Conta: {conta1.NumeroConta}");
+    Console.WriteLine($"Saldo atual: {conta1.Saldo}");
+} else
+{
+    Console.WriteLine($"Conta: {conta1.NumeroConta}");
+    Console.WriteLine($"Saldo: {conta1.Saldo}");
+}
---
+++ 03-contaBancaria/Program.cs
@@ -0,0 +1,19 @@
+﻿class ContaBancaria
+{
+    public string NumeroConta { get; set; }
+    public double Saldo { get; private set; }
+
+    public ContaBancaria(string numeroConta, double saldoInicial)
+    {
+        NumeroConta = numeroConta;
+        Saldo = saldoInicial;
+    }
+
+    public void Depositar (double valor)
+    {
+        Saldo += valor;
+    }
+
+
+
+}
