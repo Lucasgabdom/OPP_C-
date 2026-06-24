88ef0e752adb063c436d188070bd757143894a58
.\Lucasgabdom <gabriellucaswbtrombone@gmail.com.br>
2026-06-23 21:33:58 -0300

    Aprendendo como adicionar um método dentro do meu main

---
+++ 04-gestao/Program.cs
@@ -0,0 +1,24 @@
+﻿class Funcionario
+{
+    public string Nome { get; set; }
+    public string Cargo { get; set; }
+
+    public Funcionario(string nome, string cargo)
+    {
+        Nome = nome;
+        Cargo = cargo;
+    }
+
+    public void Promover(string novoCargo)
+    {
+        if (novoCargo != Cargo)
+        {
+            Cargo = novoCargo;
+            Console.WriteLine($"Funcionário {Nome} promovido para o cargo de {Cargo}.");
+        }
+        else
+        {
+            Console.WriteLine($"Funcionário {Nome} já ocupa o cargo de {Cargo}.");
+        }
+    }
+}
---
+++ 04-gestao/verificacao.cs
@@ -0,0 +1,21 @@
+﻿Funcionario funcionario1 = new Funcionario("Lucas", "Analista");
+
+Console.WriteLine($"Deseja promover o funcionário {funcionario1.Nome}? (s/n)");
+string resposta = Console.ReadLine();
+
+if (resposta == "s")
+{
+
+    Console.WriteLine($"O funcionário {funcionario1.Nome} será promovido para: ");
+    string novoCargo = Console.ReadLine();
+
+    funcionario1.Promover(novoCargo);
+
+    Console.WriteLine("\n-- Pos promoção --");
+    Console.WriteLine("Funcionário: " + funcionario1.Nome);
+    Console.WriteLine("Cargo: " + funcionario1.Cargo);
+}
+else
+{
+    Console.WriteLine("-- Promoção cancelada --");
+}
