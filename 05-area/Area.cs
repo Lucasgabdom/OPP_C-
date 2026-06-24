da6d1a756110d1ea5c17f63228fdbc75fe169d7f
.\Lucasgabdom <gabriellucaswbtrombone@gmail.com.br>
2026-06-23 21:47:35 -0300

    Adionando o retangulo

---
+++ 05-area/Area.cs
@@ -0,0 +1,11 @@
+﻿Retangulo retangulo = new Retangulo();
+
+Console.WriteLine("Por favor, informe a altura do retângulo: ");
+retangulo.Altura = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Por favor, informe a largura do retângulo: ");
+retangulo.Largura = double.Parse(Console.ReadLine());
+
+double area = retangulo.CalcularArea();
+
+Console.WriteLine($"A área do retângulo é: {area}");
---
+++ 05-area/Program.cs
@@ -0,0 +1,11 @@
+﻿class Retangulo
+{
+    public double Altura { get; set; }
+    public double Largura { get; set; }
+
+    public double CalcularArea()
+    {
+        return Altura * Largura;
+    }
+
+}
